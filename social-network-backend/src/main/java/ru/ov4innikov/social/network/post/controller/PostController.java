f955967d9392cb6192ec4030b4f3be6d88b30d64
Евгений Алексеевич Овчинников <ov4innikov.e.a@MacBook-Pro-Evgenij.local>
2024-04-29 21:06:00 +0300

    Sm refactroing

--- social-network-backend/src/main/java/ru/ov4innikov/social/network/post/controller/PostController.java
+++ social-network-backend/src/main/java/ru/ov4innikov/social/network/post/controller/PostController.java
@@ -1,10 +1,15 @@
 package ru.ov4innikov.social.network.post.controller;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.DecimalMin;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.ov4innikov.social.network.api.server.PostApi;
 import ru.ov4innikov.social.network.model.Post;
 import ru.ov4innikov.social.network.model.PostCreatePostRequest;
@@ -24,26 +29,33 @@ public class PostController implements PostApi {
 
     @Override
     public ResponseEntity<String> postCreatePost(Optional<PostCreatePostRequest> postCreatePostRequest) {
-        return null;
+        return postCreatePostRequest
+                .map(createPostRequest -> new ResponseEntity<>(postService.create(createPostRequest.getText()), HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.OK));
     }
 
     @Override
     public ResponseEntity<Void> postDeleteIdPut(String id) {
-        return null;
+        postService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<List<Post>> postFeedGet(Optional<@DecimalMin("0") BigDecimal> offset, Optional<@DecimalMin("1") BigDecimal> limit) {
-        return null;
+    public ResponseEntity<List<Post>> postFeedGet(
+            @Parameter(name = "offset", description = "", in = ParameterIn.QUERY) @Valid @RequestParam(value = "offset", required = false, defaultValue = "0") Optional<@DecimalMin("0") BigDecimal> offset,
+            @Parameter(name = "limit", description = "", in = ParameterIn.QUERY) @Valid @RequestParam(value = "limit", required = false, defaultValue = "10") Optional<@DecimalMin("1") BigDecimal> limit
+    ) {
+        return new ResponseEntity<>(postService.getFeed(offset.orElse(BigDecimal.valueOf(0L)).longValue(), limit.orElse(BigDecimal.valueOf(100L)).longValue()), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<Post> postGetIdGet(String id) {
-        return null;
+        return new ResponseEntity<>(postService.getById(id), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<Void> postUpdatePut(Optional<PostUpdatePutRequest> postUpdatePutRequest) {
-        return null;
+        postUpdatePutRequest.ifPresent((post -> postService.update(post.getId(), post.getText())));
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
